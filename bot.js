9f0d41d156447fd0f2bf49c13ce0799885e8b9d5
TorpeDoSyS <45240133+TorpeDoSyS@users.noreply.github.com>
2018-12-29 15:00:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -301,6 +301,6 @@ By MR_Phantom
 
 
 client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "498437297125982218" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "528205952449642498" && ch.type === 'voice').join();
 });
 client.login(process.env.BOT_TOKEN);
